14650fce8e70122d3cc91021f820f4c2e5c02bc1
MMarkZhang <markzhangbinglin@gmail.com>
2018-09-16 23:32:32 -0500

    Add files via upload

---
+++ Second Class/SecondPlayground.playground/Contents.swift
@@ -0,0 +1,18 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+let a = 1.0
+
+a == 1
+
+var abc = true
+var acd = false
+
+var asd = abc && acd
+
+var dsa = abc || acd
+
+asd
